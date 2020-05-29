2eb73aa0bd945c000f9c7bd257f7af5c4c4a09fc
Lionel-D <hello@lionel-d.com>
2020-05-29 15:17:40 +0200

    add basic functional test for homepage

---
+++ tests/Controller/HomepageControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomepageControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello HomepageController!');
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,33 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
+$commands = [
+    'php "%s/../bin/console" cache:clear --no-warmup',
+//    'php "%s/../bin/console" doctrine:database:drop --force',
+//    'php "%s/../bin/console" doctrine:database:create',
+//    'php "%s/../bin/console" doctrine:migrations:migrate --no-interaction',
+//    'php "%s/../bin/console" doctrine:fixtures:load --no-interaction',
+];
 
-require dirname(__DIR__).'/vendor/autoload.php';
+$prefix = '';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+if (isset($_ENV['BOOTSTRAP_LOCAL_TEST_ENV'])) {
+    $localTestEnv = $_ENV['BOOTSTRAP_LOCAL_TEST_ENV'];
+    $prefix       = 'APP_ENV=%s ';
 }
+
+foreach ($commands as $command) {
+    if ($prefix !== '') {
+        passthru(sprintf(
+            $prefix.$command,
+            $localTestEnv,
+            __DIR__
+        ));
+    } else {
+        passthru(sprintf(
+            $command,
+            __DIR__
+        ));
+    }
+}
+
+require __DIR__.'/../config/bootstrap.php';
