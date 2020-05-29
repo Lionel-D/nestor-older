327156fd38af7be54ccdbf0322374ce5bff83cf4
Lionel-D <hello@lionel-d.com>
2020-05-29 10:56:15 +0200

    add missing annotation

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class HomepageController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     * @return Response
      */
     public function index()
     {
