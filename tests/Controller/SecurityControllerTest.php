25e7960558841709e03acdce0a176b55aa5072ad
Lionel-D <hello@lionel-d.com>
2020-06-08 09:05:32 +0200

    add login test as already authenticated

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -29,6 +29,17 @@ final class SecurityControllerTest extends AppTestCase
         $this->assertSelectorTextContains('h1', 'This is Nestor');
     }
 
+    public function testLoginAsAlreadyAuthenticated(): void
+    {
+        $this->assertLoggedAsUser();
+
+        $this->kernelBrowser->request('GET', '/login');
+
+        $this->assertResponseRedirects('/app/dashboard');
+        $this->kernelBrowser->followRedirect();
+        $this->assertSelectorTextContains('h1', 'Welcome Lionel!');
+    }
+
     private function successfullyLoadLoginPage(): Crawler
     {
         $crawler = $this->kernelBrowser->request('GET', '/login');
